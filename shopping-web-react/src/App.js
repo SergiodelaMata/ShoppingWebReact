3fe324ffbc8765d028c152f439c9d10557c66153
SergiodelaMata <sergio.matam@edu.uah.es>
2022-03-22 19:25:09 +0100

    :sparkles: Saved the data from the json file in state.

--- shopping-web-react/src/App.js
+++ shopping-web-react/src/App.js
@@ -1,10 +1,42 @@
 import React, { Component } from 'react';
 import './App.css';
+import JsonFile from './files/test.json';
 
 import bootstrap from 'bootstrap/dist/css/bootstrap.min.css'
 import Navbar from './components/Navbar'
 
 export class App extends Component {
+  state = {
+    user: "",
+    page: "",
+    categories: [],
+    products: [],
+    users: []
+  }
+
+  constructor(props)
+  {
+    super(props);
+    this.getData = this.getData.bind(this);
+  }
+
+  componentDidMount() {
+    window.addEventListener('load', this.getData);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('load', this.getData);
+  }
+
+  getData=()=>{
+    const json = JSON.parse(JSON.stringify({JsonFile})).JsonFile;
+    console.log(json.categories);
+    this.setState({
+      categories: json.categories,
+      products: json.products,
+      users: json.users
+    })
+  }
 
   render(){
     return (
